4a8cecafedf7ba2958cbbc44c1091cc38b910bea
Joel Pablo <dgtlmonk@gmail.com>
2020-03-01 19:01:12 +0800

    Update firebase.js

--- config/firebase.js
+++ config/firebase.js
@@ -1,10 +1,10 @@
 module.exports = {
-  apiKey: "AIzaSyB0MYdD5HHnChxk8dFLAz_X8qNo3pd-Oy0",
+  apiKey: "yourAPIKeyHere",
   authDomain: "dgtlmonkstudio-d95a8.firebaseapp.com",
   databaseURL: "https://dgtlmonkstudio-d95a8.firebaseio.com",
-  projectId: "dgtlmonkstudio-d95a8",
-  storageBucket: "dgtlmonkstudio-d95a8.appspot.com",
-  messagingSenderId: "781702222550",
-  appId: "1:781702222550:web:c84beaceb2696fdad4af77",
-  measurementId: "G-3VFRK1ZG35"
+  projectId: "yourProjectId",
+  storageBucket: "yourStorageBucket",
+  messagingSenderId: "yourMessaringSenderIdHere",
+  appId: "yourAppIdHere",
+  measurementId: "yourMeasurementIdHere"
 };
